8201c9919a05dbd5d9f75cdaa9cd61dd6178a3b3
Ramon Prata <ramon.prata@dtidigital.com.br>
2024-05-09 19:23:52 -0300

    setup folder structure

--- src/_app/index.ts
+++ src/_app/index.ts
@@ -1,3 +1,4 @@
 export { default as RootLayout } from './views/layout/RootLayout';
 export { default as MainTabs } from './views/navigations/MainTabs';
 export { default as MainStack } from './views/navigations/MainStack';
+export { default as NotFoundScreen } from './views/layout/NotFoundScreen';
